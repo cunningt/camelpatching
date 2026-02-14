55b8d424ee665b2899a13d8c43a1e33ec75a8c5a
Tom Cunningham <tcunning@redhat.com>
2026-02-13 19:45:00 -0500

    Fix patches for camel-4.18.0

--- csb.pl
+++ csb.pl
@@ -4,12 +4,12 @@ $ENV{JAVA_HOME} = "/opt/homebrew/Cellar/openjdk@21/21.0.8/libexec/openjdk.jdk/Co
 
 my $endpoint = defined($ARGV[0]) ? shift(@ARGV) : "";
 
-$vers = "4.14.4";
+$vers = "4.18.0";
 $dir = "camel-spring-boot-${vers}-branch";
 $patchdir = "csbpatches";
 
 $upstreambranch = "camel-spring-boot-$vers";
-$currentprodbranch = "camel-spring-boot-4.14.2-branch";
+$currentprodbranch = "camel-spring-boot-4.14.4-branch";
 $prodlocation = "csbprodlocation";
 
 # Clean up directories
